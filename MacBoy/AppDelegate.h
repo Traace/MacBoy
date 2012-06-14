50f01cde3be0d5ed2c02b44abb5acf4b42f57abe
Tom Schroeder <j.tom.schroeder@gmail.com>
2012-06-14 16:26:38 -0500

    Initial Commit

---
+++ MacBoy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MacBoy
+//
+//  Created by Tom Schroeder on 6/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MacBoy/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  MacBoy
+//
+//  Created by Tom Schroeder on 6/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+   // Insert code here to initialize your application
+}
+
+@end
